043313e85794633bfb122b6ea6bca2e853e78cab
vivekneupane11 <vivekneupane11@gmail.com>
2023-09-23 19:09:24 +0545

    added google fonts

--- App.tsx
+++ App.tsx
@@ -1,14 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import { Mulish_500Medium, useFonts } from '@expo-google-fonts/mulish';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { THEME } from './constants/Theme';
+
 export default function App() {
- 
+  let [fontsLoaded, fontError] = useFonts({
+    Mulish_500Medium,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text >Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontFamily: 'Mulish_500Medium', fontSize: 14,color:THEME.COLORS.primaryTextColor }}>mulish</Text>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {

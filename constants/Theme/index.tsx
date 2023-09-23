67318daf4ea1977f630b5e636c83ca297dcfe596
vivekneupane11 <vivekneupane11@gmail.com>
2023-09-23 18:59:12 +0545

    init constats and images

---
+++ constants/Theme/index.tsx
@@ -0,0 +1,21 @@
+export const THEME ={
+    COLORS:{
+        primary: "#06373D",
+        primaryTextColor:'#111515',
+        secondaryDarkTextColor:"#666666",
+        secondaryLightTextColor:"#A3A3A3",
+
+    },
+    IMAGES:{
+        ARROW:require('../../assets/Onboard/arrow.png'),
+        CHECK:require('../../assets/Onboard/check.png'),
+        LOGO:require('../../assets/Onboard/logo.png'),
+        RECOVERY_PASSWORD:require('../../assets/Onboard/recovery-password.png'),
+        SET_PASSWORD:require('../../assets/Onboard/set-password.png'),
+        UNDERLINE_SIGN_IN:require('../../assets/Onboard/underline-signin.png'),
+        USER_LOGO_CHECK:require('../../assets/Onboard/user-logo-check.png'),
+        USER_LOGO:require('../../assets/Onboard/userlogo.png'),
+
+
+    }
+}

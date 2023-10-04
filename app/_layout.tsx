bdfbfb9781ab35c989ad5d3d9d65ae41d1683e09
vivekneupane11 <vivekneupane11@gmail.com>
2023-10-04 21:11:29 +0545

    minor

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,85 +1,55 @@
+import Entypo from '@expo/vector-icons/Entypo';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
 
-import { Text, View } from "react-native";
-// SplashScreen.preventAutoHideAsync();
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
-const StackLayout = () => {
-  // const router = useRouter();
-  // let [fontsLoaded, fontError] = useFonts({
-  //   Mulish_500Medium,
-  //   Mulish_300Light,
-  //   Mulish_400Regular,
-  //   Mulish_600SemiBold,
-  //   Mulish_700Bold,
-  //   Mulish_900Black,
-  // });
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
 
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded ) {
-   
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [fontsLoaded]);
+    prepare();
+  }, []);
 
-  // if (!fontsLoaded && !fontError) {
-  //   return null;
-  // }
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
 
   return (
-    <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'yellow'}}>
-<Text>Hello</Text>
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}>
+      <Text>SplashScreen Demo! 👋</Text>
+      <Entypo name="rocket" size={30} />
     </View>
-    // <Stack
-    //   screenOptions={{
-    //     headerStyle: {
-    //       backgroundColor: "#10101E",
-    //     },
-    //     headerTintColor: "#fff",
-    //     headerTitleStyle: {
-    //       fontWeight: "bold",
-    //     },
-    //   }}
-    // >
-    //   <Stack.Screen
-    //     name="index"
-    //     options={{ headerTitle: "Login", headerShown: false }}
-    //   />
-    //   <Stack.Screen
-    //     name="forgetpassword"
-    //     options={{ headerTitle: "Password Recovery", headerShown: false }}
-    //   />
-    //    <Stack.Screen
-    //     name="chat"
-    //     options={{ headerTitle: "Chat", headerShown: false }}
-    //   />
-    //    <Stack.Screen
-    //     name="otp"
-    //     options={{ headerTitle: "OTP Verify", headerShown: false }}
-    //   />
-    //      <Stack.Screen
-    //     name="verify"
-    //     options={{ headerTitle: " Verify", headerShown: false }}
-    //   />
-    //     <Stack.Screen
-    //     name="newpassword"
-    //     options={{ headerTitle: "New Password", headerShown: false }}
-    //   />
-    //     <Stack.Screen
-    //     name="register"
-    //     options={{ headerTitle: "Register", headerShown: false }}
-    //   />
-    //   <Stack.Screen
-    //     name="chooseprofile"
-    //     options={{ headerTitle: "Choose Profile", headerShown: false }}
-    //   />
-    //   <Stack.Screen
-    //     name="(tabs)"
-    //     options={{
-    //       headerShown: false,
-    //     }}
-    //   />
-    // </Stack>
   );
-};
-
-export default StackLayout;
+}

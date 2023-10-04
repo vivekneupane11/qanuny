1857c2389fac40e58bd1087cb0508591abf52eaf
vivekneupane11 <vivekneupane11@gmail.com>
2023-10-04 18:24:36 +0545

    minor

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,92 +1,84 @@
-import {
-  Mulish_300Light,
-  Mulish_400Regular,
-  Mulish_500Medium,
-  Mulish_600SemiBold,
-  Mulish_700Bold,
-  Mulish_900Black,
-  useFonts,
-} from "@expo-google-fonts/mulish";
-import { Stack, useRouter } from "expo-router";
-import * as SplashScreen from 'expo-splash-screen';
 
-import { useCallback } from "react";
-SplashScreen.preventAutoHideAsync();
+import { Text, View } from "react-native";
+// SplashScreen.preventAutoHideAsync();
 
 const StackLayout = () => {
-  const router = useRouter();
-  let [fontsLoaded, fontError] = useFonts({
-    Mulish_500Medium,
-    Mulish_300Light,
-    Mulish_400Regular,
-    Mulish_600SemiBold,
-    Mulish_700Bold,
-    Mulish_900Black,
-  });
+  // const router = useRouter();
+  // let [fontsLoaded, fontError] = useFonts({
+  //   Mulish_500Medium,
+  //   Mulish_300Light,
+  //   Mulish_400Regular,
+  //   Mulish_600SemiBold,
+  //   Mulish_700Bold,
+  //   Mulish_900Black,
+  // });
 
 
-  const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded ) {
+  // const onLayoutRootView = useCallback(async () => {
+  //   if (fontsLoaded ) {
    
-      await SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
+  //     await SplashScreen.hideAsync();
+  //   }
+  // }, [fontsLoaded]);
 
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
+  // if (!fontsLoaded && !fontError) {
+  //   return null;
+  // }
 
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#10101E",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{ headerTitle: "Login", headerShown: false }}
-      />
-      <Stack.Screen
-        name="forgetpassword"
-        options={{ headerTitle: "Password Recovery", headerShown: false }}
-      />
-       <Stack.Screen
-        name="chat"
-        options={{ headerTitle: "Chat", headerShown: false }}
-      />
-       <Stack.Screen
-        name="otp"
-        options={{ headerTitle: "OTP Verify", headerShown: false }}
-      />
-         <Stack.Screen
-        name="verify"
-        options={{ headerTitle: " Verify", headerShown: false }}
-      />
-        <Stack.Screen
-        name="newpassword"
-        options={{ headerTitle: "New Password", headerShown: false }}
-      />
-        <Stack.Screen
-        name="register"
-        options={{ headerTitle: "Register", headerShown: false }}
-      />
-      <Stack.Screen
-        name="chooseprofile"
-        options={{ headerTitle: "Choose Profile", headerShown: false }}
-      />
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <View style={{flex:1,justifyContent:'center',alignItems:'center',backgroundColor:'yellow'}}>
+<Text>Hello</Text>
+    </View>
+    // <Stack
+    //   screenOptions={{
+    //     headerStyle: {
+    //       backgroundColor: "#10101E",
+    //     },
+    //     headerTintColor: "#fff",
+    //     headerTitleStyle: {
+    //       fontWeight: "bold",
+    //     },
+    //   }}
+    // >
+    //   <Stack.Screen
+    //     name="index"
+    //     options={{ headerTitle: "Login", headerShown: false }}
+    //   />
+    //   <Stack.Screen
+    //     name="forgetpassword"
+    //     options={{ headerTitle: "Password Recovery", headerShown: false }}
+    //   />
+    //    <Stack.Screen
+    //     name="chat"
+    //     options={{ headerTitle: "Chat", headerShown: false }}
+    //   />
+    //    <Stack.Screen
+    //     name="otp"
+    //     options={{ headerTitle: "OTP Verify", headerShown: false }}
+    //   />
+    //      <Stack.Screen
+    //     name="verify"
+    //     options={{ headerTitle: " Verify", headerShown: false }}
+    //   />
+    //     <Stack.Screen
+    //     name="newpassword"
+    //     options={{ headerTitle: "New Password", headerShown: false }}
+    //   />
+    //     <Stack.Screen
+    //     name="register"
+    //     options={{ headerTitle: "Register", headerShown: false }}
+    //   />
+    //   <Stack.Screen
+    //     name="chooseprofile"
+    //     options={{ headerTitle: "Choose Profile", headerShown: false }}
+    //   />
+    //   <Stack.Screen
+    //     name="(tabs)"
+    //     options={{
+    //       headerShown: false,
+    //     }}
+    //   />
+    // </Stack>
   );
 };
 
